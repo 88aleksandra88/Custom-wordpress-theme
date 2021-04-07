5f5d381db7f58a13b7afd4bf31479fb425635758
Aleks <aleksandra.slowik88@gmail.com>
2021-04-07 15:46:41 +0200

    add database

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         '0G8j`kRwzJx->NC=.)jer`jrL.[$( 6) XAp8#yJKh@QV<H-BHi3u9fz|%b{j-ab' );
+define( 'SECURE_AUTH_KEY',  'bYHXh@oF?*,1{S_%zPU8gegvz)2tj0dVPXYWQ@Wn2K=]fcw~a;F`=RuAr3M=JH!F' );
+define( 'LOGGED_IN_KEY',    'e,E:mNMg{Zm@3^~!|>UAPdlq=lqG<Jt35@>nN>@9i:qv(MySBA{,HB.;]KvOZXBa' );
+define( 'NONCE_KEY',        '3>FAa-bs)s4**EMI lsFB-V3:ox$CZJSMk`+/_E+gf@?A [.0Vi~2){rck@Ks<:.' );
+define( 'AUTH_SALT',        'kV:[xG6w_(:yOKk*k*I:kH.|0H!^Fc/HfsJI7M}~ivl8`8:4fs>p52V3dK_M;4l ' );
+define( 'SECURE_AUTH_SALT', 'Z=JP[.aklJww]StMmA},%hmb5MN}*Lq4 &NRyi3-$7}?e+T?MI5&|({BU!Wy<Q)-' );
+define( 'LOGGED_IN_SALT',   'UFyz?hlLOs7Aj?oO 6spKlGEX5.aZ6D3^al9;H4%P?`raZF_4@Hzqj,tk[vJ=aFR' );
+define( 'NONCE_SALT',       '{.t2c,#S~N]ISCe5<W}XGjk*;a/8p{RE*KTxUpfkeZ*)NkI%DG]-tri62> LELj7' );
 
 /**#@-*/
 
